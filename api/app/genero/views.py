fe45e04569cc8ae5b285bc0f16718c3eaa859717
Eduardo Medeiros Pereira <edu.medeirospereira@gmail.com>
2020-09-20 15:13:57 -0300

    endpoint para recuperacao do genero pelo codigo

--- api/app/genero/views.py
+++ api/app/genero/views.py
@@ -1,10 +1,23 @@
 from flask import Blueprint
 from .. import BLUEPRINTS
+from flask import current_app as app
+from werkzeug.exceptions import NotFound
 
 genero_bp = Blueprint('genero', __name__)
 
 BLUEPRINTS.append(genero_bp)
 
-@genero_bp.route('/genero/test', methods=['GET'])
-def get_test():
-    return "TESTE MODULARIZACAO COM BLUEPRINTS"
+@genero_bp.route('/genero/codigo/<codigo>', methods=['GET'])
+def get_por_codigo(codigo):
+
+    generoCollection = app.data.driver.db['genero']
+
+    genero = generoCollection.find_one({"codigo":str(codigo)})
+
+    if genero == None:
+        raise NotFound
+
+    #convertendo o ObjectId do pymongo para string, tornando-o serializavel    
+    genero['_id'] = str(genero['_id'])
+    
+    return genero
